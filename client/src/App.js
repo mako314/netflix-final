250a55e0ea6a668c087d85ac3768926760c23d0f
Macolister <bispo.mac@gmail.com>
2024-01-08 18:18:06 -0500

    user, favorites, and movies fetches state

---
+++ client/src/App.js
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+  const [moviesData, setMoviesData] = useState([])
+  const [usersData, setUsersData] = useState([])
+  const [favoritesData, setFavoritesData] = useState([])
+
+  useEffect(() => {
+  // let ignore = false
+  //------------------------------------- Fetch for Movies ----------------
+
+    fetch(`http://127.0.0.1:5555/movies`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+    }).then((resp) => {
+      // console.log(resp)
+        if (resp.ok) {
+            // If the response is OK
+            resp.json().then((data) => {
+              setMoviesData(data)
+            })
+        } else {
+            // If the response is not OK, handle errors
+            resp.json().then((errorData) => {
+            console.error('Error Response:', errorData)
+            })
+        }
+    }).catch((error) => {
+        //  catch network errors and other issues with the fetch request
+        console.error('Fetch Error:', error)
+    })
+
+//------------------------------------- Fetch for Users ----------------
+    fetch(`http://127.0.0.1:5555/users`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+    }).then((resp) => {
+      //console.log(resp)
+        if (resp.ok) {
+            // If the response is OK
+            resp.json().then((data) => {
+              setUsersData(data)
+            })
+        } else {
+            // If the response is not OK, handle errors
+            resp.json().then((errorData) => {
+            console.error('Error Response:', errorData)
+            })
+        }
+    }).catch((error) => {
+        //  catch network errors and other issues with the fetch request
+        console.error('Fetch Error:', error)
+    })
+
+  //----------------------FETCH FOR FAVORITES-----------------------------
+
+  fetch(`http://127.0.0.1:5555/favorites`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    }).then((resp) => {
+      //console.log(resp)
+        if (resp.ok) {
+            // If the response is OK
+            resp.json().then((data) => {
+              setFavoritesData(data)
+            })
+        } else {
+            // If the response is not OK, handle errors
+            resp.json().then((errorData) => {
+            console.error('Error Response:', errorData)
+            })
+        }
+    }).catch((error) => {
+        //  catch network errors and other issues with the fetch request
+        console.error('Fetch Error:', error)
+    })
+  }, [])
+
+  console.log("THE MOVIE DATA STATE:", moviesData)
+  console.log("THE USER DATA STATE:", usersData)
+  console.log("THE FAVORITE DATA STATE:", favoritesData)
+
+  return (
+    <div className="App">
+      Test West
+    </div>
+  )
+}
+
+export default App
