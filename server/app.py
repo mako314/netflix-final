498d0a0f833798f3959ea05f06fe0731d74d4af9
DeltaLambda77 <danlozano77@gmail.com>
2024-01-09 23:02:24 -0500

    TVSeriesByActor route added

--- server/app.py
+++ server/app.py
@@ -217,6 +217,29 @@ class TVSeriesById(Resource):
 
 api.add_resource(TVSeriesById, '/tv-series/<int:id>')
 
+class TVSeriesByActor(Resource):
+    def get(self, actor):
+        array_of_tv_series = []
+
+        all_tv_series = TelevisionSeries.query.all()
+
+        for tv_series in all_tv_series:
+            if actor.lower() in tv_series.stars.lower():
+                array_of_tv_series.append(tv_series)
+
+        tv_series_to_send = [tv_series.to_dict(rules=(not_needed_data)) for tv_series in array_of_tv_series]
+
+        if tv_series_to_send:
+            response = make_response(tv_series_to_send, 200)
+        else:
+            response = make_response({
+                "error": f"TV Series containing the following actor: {actor} was not found"
+            }, 404)
+        
+        return response
+    
+api.add_resource(TVSeriesByActor, '/tv-series/<string:actor>/actors')
+
 class Favorites(Resource):
     def get(self):
         favorites = [favorite.to_dict() for favorite in Favorite.query.all()]
