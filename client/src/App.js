ed420818cc02cad612c0ce28ea6f90da4b2e2099
DeltaLambda77 <danlozano77@gmail.com>
2024-01-15 15:53:03 -0500

    Setup TVSeries state + api call

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ function App() {
   const [moviesData, setMoviesData] = useState([])
   const [usersData, setUsersData] = useState([])
   const [favoritesData, setFavoritesData] = useState([])
+  const [tvSeriesData, setTVSeriesData] = useState([]);
 
   useEffect(() => {
   // let ignore = false
@@ -89,11 +90,33 @@ function App() {
         //  catch network errors and other issues with the fetch request
         console.error('Fetch Error:', error)
     })
+
+    //----------------------FETCH FOR TV SERIES-----------------------------
+    fetch(`http://127.0.0.1:5555/tv-series`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      if (response.ok) {
+        response.json().then((data) => {
+          setTVSeriesData(data)
+        })
+      }
+      else {
+        response.json().then((errorData) => {
+          console.error('Error Response:', errorData)
+        })
+      }
+    }).catch((error) => {
+      console.error('Fetch Error:', error)
+    })
   }, [])
 
   console.log("THE MOVIE DATA STATE:", moviesData)
   console.log("THE USER DATA STATE:", usersData)
   console.log("THE FAVORITE DATA STATE:", favoritesData)
+  console.log("THE TV SERIES DATA STATE:", tvSeriesData)
 
   return (
     <div className="App">
