7c2b38b86c47b3d225b6e8af694f9fc770120f55
DeltaLambda77 <danlozano77@gmail.com>
2024-01-15 23:18:18 -0500

    added some jwt stuff to config.py

--- server/config.py
+++ server/config.py
@@ -6,11 +6,23 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from sqlalchemy import MetaData
 # from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
 import os
 
 # load_dotenv('../.env.local')
 
 app = Flask(__name__)
+
+# allows JWTs to be sent to the web app via cookies
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+
+# Change to true for prod. When true, will only allow JWTs encapsulated within cookies to be sent over https
+app.config["JWT_COOKIE_SECURE"] = False
+
+# Should probably route to some env file secret key
+app.config["JWT_SECRET_KEY"] = "super-secret-key"
+
+jwt = JWTManager(app)
 
 # app.secret_key = os.getenv('FLASK_SECRET_KEY') # signature for Flask session
 app.secret_key = "TESTING123456789"
