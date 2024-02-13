1b1af4404f92ec2c523a153532ae206f5ee2b96f
DeltaLambda77 <danlozano77@gmail.com>
2024-02-12 20:51:32 -0500

    keeping commit up to date

--- client/src/Admin/AdminDashboard.js
+++ client/src/Admin/AdminDashboard.js
@@ -6,10 +6,24 @@ function AdminDashboard() {
     const { currentUser } = CurrentUserContext();
 
     return (
-        <>
+        <div>
             <p>Hi {currentUser.first_name}</p>
+            <div>
+                <p>Need to add a way to retrieve all users... maybe delete them</p>
+                <p>Need to add a way to edit existing movies and tv_series</p>
+                <p>Need to add a way to add new tv series and movies</p>
+            </div>
 
-        </>
+            <div className="m-6">
+                <p>User Div</p>
+            </div>
+            <div className="m-6">
+                <p>Movie Div</p>
+            </div>
+            <div className="m-6">
+                <p>TV Series Div</p>
+            </div>
+        </div>
     )
 }
 
