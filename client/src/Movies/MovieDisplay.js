66864942d106394b93edfd7bf0707640cb205fa8
Macolister <bispo.mac@gmail.com>
2024-02-18 20:43:24 -0500

    thumbnail placeholder for movie display toggle for video

--- client/src/Movies/MovieDisplay.js
+++ client/src/Movies/MovieDisplay.js
@@ -1,14 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 
 function MovieDisplay({moviesData}){
     // const { id } = useParams()
     // const movieIndexValue = id - 1
 
-    // console.log("THE MOVIE ID:", id)
-    // console.log("movie index value:", movieIndexValue)
-    // const navigate = useNavigate()
-
+    const [showVideo, setShowVideo] = useState(false)
     const location = useLocation()
     const { fullMovie } = location.state || {}
 
@@ -16,22 +13,37 @@ function MovieDisplay({moviesData}){
 
     console.log("IN MOVIE DISPLAY:", fullMovie)
 
+    const toggleVideo = () => {
+        setShowVideo(!showVideo)
+    }
+
 
     return (
         <div className="flex flex-grow items-center justify-center bg-white min-h-screen">
             <div className="bg-white shadow-lg rounded-lg max-w-6xl w-full overflow-hidden m-4">
-                {/* Video Section */}
-                {/* <div className="aspect-w-16 aspect-h-9 w-full overflow-hidden rounded-lg">
-                    <video
-                        controls
-                        onLoadedMetadata={(e) => e.target.volume = 0.2}
-                        className="w-full h-auto"
-                        aria-label={`Watch ${fullMovie.title}`}
-                    >
-                        <source src="https://d3th70t1rge79u.cloudfront.net/arthur/001-ArthurEyes.mp4" type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
-                </div> */}
+                {/* Conditional Rendering for Thumbnail or Video */}
+                {showVideo ? (
+                    <div className="aspect-w-16 aspect-h-9 w-full overflow-hidden rounded-lg">
+                        <video
+                            controls
+                            onLoadedMetadata={(e) => e.target.volume = 0.2}
+                            className="w-full h-auto"
+                            aria-label={`Watch ${fullMovie.title}`}
+                        >
+                            <source src="https://d3th70t1rge79u.cloudfront.net/arthur/001-ArthurEyes.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                ) : (
+                    <div className="flex justify-center" onClick={toggleVideo}> 
+                        <img 
+                        src={fullMovie.thumbnail} 
+                        alt={`Watch ${fullMovie.title}`} 
+                        className="w-full h-full object-cover cursor-pointer" 
+                        style={{ maxWidth: '50%', maxHeight: '50%' }}
+                        />
+                    </div>
+                )}
     
                 {/* Movie Details */}
                 <div className="p-4 lg:p-8">
@@ -47,19 +59,25 @@ function MovieDisplay({moviesData}){
                     <p className="mt-4 text-indigo-500 font-semibold">{fullMovie.genres}</p>
                 </div>
     
-                {/* Action Button */}
-                <div className="flex justify-center p-4 lg:p-8 bg-gray-200">
-                    <button
-                        type="button"
-                        className="rounded hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 px-4 py-2 bg-indigo-500 text-white text-lg transition duration-150 ease-in-out"
-                    >
-                        Watch Now
-                    </button>
-                </div>
+                {/* Toggle Button for Video */}
+                {!showVideo && (
+                    <div className="flex justify-center p-4 lg:p-8 bg-gray-200">
+                        <button
+                            type="button"
+                            className="rounded hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 px-4 py-2 bg-indigo-500 text-white text-lg transition duration-150 ease-in-out"
+                        >
+                            Watch Trailer
+                            Need button / modal
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     )
 }
 
 export default MovieDisplay;
+
+
+
 
