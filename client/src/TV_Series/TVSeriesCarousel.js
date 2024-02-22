4db45715555a0668299d36b86b61d961fa18042b
Macolister <bispo.mac@gmail.com>
2024-02-22 14:34:25 -0500

    alphabetical sorting for tv series

--- client/src/TV_Series/TVSeriesCarousel.js
+++ client/src/TV_Series/TVSeriesCarousel.js
@@ -6,6 +6,7 @@ import { useParams, useNavigate, useLocation } from "react-router-dom";
 
 function TVSeriesCarousel({ tvSeriesData, homePage }) {
     const [dateSort, setDateSort] = useState('newest')
+    const [alphabetSort, setAlphabetSort] = useState(true)
 
 
     const navigate = useNavigate()
@@ -21,6 +22,14 @@ function TVSeriesCarousel({ tvSeriesData, homePage }) {
         setDateSort(event.target.value)
     }
 
+    const handleAlphabetSort = (event) => {
+        // console.log('Selected value:', event.target.value)
+        if (event.target.value === "true"){
+            return setAlphabetSort(true)
+        }
+        setAlphabetSort(false)
+    }
+
     let userSortOption = dateSort === 'newest' ?
     ((a, b) => new Date(b.release_date) - new Date(a.release_date)) : // Newest first
     ((a, b) => new Date(a.release_date) - new Date(b.release_date)) // Oldest first
@@ -29,20 +38,53 @@ function TVSeriesCarousel({ tvSeriesData, homePage }) {
 
     const sortedSeries = tvSeriesData.sort(userSortOption)
 
+    // https://stackoverflow.com/questions/6712034/sort-array-by-firstname-alphabetically-in-javascript
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
+    // https://stackoverflow.com/questions/48111425/sort-objects-in-an-array-alphabetically-based-on-one-property
+    // https://stackoverflow.com/questions/54298232/sorting-an-array-of-objects-alphabetically
+
+    const alphabeticalSort = sortedSeries.sort(function (a, b) {
+        var textA = a.title.toUpperCase();
+        var textB = b.title.toUpperCase();
+      
+        return alphabetSort ? textA.localeCompare(textB) : textB.localeCompare(textA);
+    })
+
+    
+
+
+
     return (
         <div className="max-w-full mx-auto overflow-hidden mt-4 ml-4">
             {/* <label for="foods">What do you want to eat?</label><br /> */}
-            <select 
+            <div className="flex flex-row"> 
+        <select 
                 className="block appearance-none w-auto bg-white border border-gray-300 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 my-2"
                 value={dateSort}
                 onChange={handleDateSort}
-            >
+        >
                 <option value="" disabled>--Please choose an option--</option>
                 <option name="newest_option" value="newest" id="newest">Newest Series</option>
                 <option name="oldest_option" value="oldest" id="oldest">Oldest Series</option>
         </select>
+
+        <select 
+                className="block appearance-none w-auto bg-white border border-gray-300 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 my-2 ml-4"
+                value={alphabetSort}
+                onChange={handleAlphabetSort}
+        >
+                <option value="" disabled>--Please choose an option--</option>
+                <option name="newest_option" value="true" id="alphabetTrue">A-Z</option>
+                <option name="oldest_option" value="false" id="alphabetFalse">Z-A</option>
+        </select>
+
+        </div>
+
+        
+
+
             {genres.map((genre) => {
-                const filteredTvSeries = sortedSeries.filter(tvSeries => 
+                const filteredTvSeries = alphabeticalSort.filter(tvSeries => 
                     tvSeries.genres.toLowerCase().includes(genre)
                 )
 
