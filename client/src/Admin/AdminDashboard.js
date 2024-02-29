4b1a04c84dd0398c79f8be9f620c9f33f908b5e3
DeltaLambda77 <danlozano77@gmail.com>
2024-02-29 17:38:50 -0500

    setting up UserModal

--- client/src/Admin/AdminDashboard.js
+++ client/src/Admin/AdminDashboard.js
@@ -3,9 +3,143 @@ import ApiUrlContext from "../Api";
 import { CurrentUserContext } from "../UserLogin/UserContext";
 import AccordionItem from "./AccordionItem";
 
-const UserDataTable = ({usersData, deleteUser}) => {
+const UserModal = ({showModal, toggleModal, selectedUserID}) => {
+
+  const submitForm = (selectedUserID) => {
+    console.log(selectedUserID);
+  }
+
+  return (
+    <>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Edit User
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={(showModal) => toggleModal}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <form onSubmit={() => submitForm(selectedUserID)}>
+                    <div className="flex flex-row">
+                      <label htmlFor="firstName" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">First Name</label>
+                      <input type="text" name="first_name" className="w-50% rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+
+                    <div className="flex flex-row">
+                      <label htmlFor="lastName" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Last Name</label>
+                      <input type="text" name="last_name" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+
+                    <div className="flex flex-row">
+                      <label htmlFor="email" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Email</label>
+                      <input type="email" name="email" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+
+                    <div className="flex flex-row">
+                      <label htmlFor="phone" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Phone Number</label>
+                      <input type="text" name="phone" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+
+                    <div className="flex flex-row">
+                      <label htmlFor="password" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Password</label>
+                      <input type="password" name="password" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+
+                    <div className="flex flex-row">
+                      <label htmlFor="date_of_birth" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Date of Birth</label>
+                      <input type="text" name="date_of_birth" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+
+                    <div className="flex flex-row">
+                      <label htmlFor="profile_image" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Profile Image</label>
+                      <input type="text" name="profile_image" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+
+                    <div className="flex flex-row">
+                      <label htmlFor="country" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Country</label>
+                      <input type="text" name="country" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+
+                    <div className="flex flex-row">
+                      <label htmlFor="state" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">State</label>
+                      <input type="text" name="state" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+                    
+                    <div className="flex flex-row">
+                      <label htmlFor="city" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">City</label>
+                      <input type="text" name="city" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+
+                    <div className="flex flex-row">
+                      <label htmlFor="address_line_1" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Address Line 1</label>
+                      <input type="text" name="address_line_1" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+
+                    <div className="flex flex-row">
+                      <label htmlFor="address_line_2" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Address Line 2</label>
+                      <input type="text" name="address_line_2" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+
+                    <div className="flex flex-row">
+                      <label htmlFor="postal_code" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Postal Code</label>
+                      <input type="text" name="postal_code" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                    </div>
+                  </form>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => toggleModal(showModal)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="submit"
+                    onClick={() => toggleModal(showModal)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
+
+const UserDataTable = ({usersData, deleteUser, UserModal}) => {
 
   const apiUrl = process.env.REACT_APP_API_URL;
+
+  const [showModal, setShowModal] = useState(false);
+  const [selectedUserID, setSelectedUserID] = useState("");
+
+  const toggleModal = (showModal, userID) => {
+    setShowModal(!showModal);
+    setSelectedUserID(userID);
+  }
 
   function patchUser(userID) {
     console.log(usersData);
@@ -78,7 +212,7 @@ const UserDataTable = ({usersData, deleteUser}) => {
                 <td>{user.email}</td>
                 <td>{user.date_of_birth}</td>
                 <td>
-                  <button className="bg-green-500" type="button" onClick={() => patchUser(user.id)}>
+                  <button className="bg-green-500" type="button" onClick={() => toggleModal(showModal)}>
                     &#9999;&#65039;
                   </button>
                 </td>
@@ -93,6 +227,8 @@ const UserDataTable = ({usersData, deleteUser}) => {
           }
 
         </tbody>
+
+        <UserModal showModal={showModal} toggleModal={toggleModal} patchUser={patchUser} selectedUserID={selectedUserID}/>
       </div>
     </div>
     
@@ -704,6 +840,8 @@ function AdminDashboard({ usersData, tvSeriesData, moviesData }) {
 		},
 	];
 
+
+
   return (
       <div>
           {accordionData.map((item, index) => (
@@ -725,7 +863,7 @@ function AdminDashboard({ usersData, tvSeriesData, moviesData }) {
           {index === 0 &&
             <div>
               <AccordionItem 
-                contents={<UserDataTable usersData={usersData} deleteUser={deleteUser}/>}
+                contents={<UserDataTable usersData={usersData} deleteUser={deleteUser} UserModal={UserModal}/>}
                 title={"All DNN Users"}
               />
               <AccordionItem 
